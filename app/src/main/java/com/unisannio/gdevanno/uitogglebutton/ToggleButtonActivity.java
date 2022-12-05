add061372c955890105157a6bb76b09cd1f344ae
Giovanni De Vanno <giovannidv96@gmail.com>
2022-12-05 17:17:15 +0100

    Creazione classe e layout ToggleButtonActivity

---
+++ app/src/main/java/com/unisannio/gdevanno/uitogglebutton/ToggleButtonActivity.java
@@ -0,0 +1,14 @@
+package com.unisannio.gdevanno.uitogglebutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ToggleButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toggle_button);
+    }
+}
